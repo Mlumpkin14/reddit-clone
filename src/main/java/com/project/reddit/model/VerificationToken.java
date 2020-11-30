06835433e80c332d70fe2713adc8df156936acf0
Mlumpkin14 <mattlumpkin04@gmail.com>
2020-11-30 02:14:02 -0500

    Added all Models Added SpringRedditException

---
+++ src/main/java/com/project/reddit/model/VerificationToken.java
@@ -0,0 +1,31 @@
+package com.project.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.Instant;
+
+import static java.awt.font.TransformAttribute.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private Instant expiryDate;
+}
+
